312ba74bff104e96924b7ec96a4ad0778c64694a
Kiel Byrne <kbyrne@costar.com>
2021-01-16 00:34:04 -0500

    add opengraph key, need to add atlas keys

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const path = require('path')
 
 module.exports = {
   env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY
+    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    NEXT_PUBLIC_OG_KEY: process.env.NEXT_PUBLIC_OG_KEY
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
