6caa68afda81fe043b40fc772967bee8c1b3584c
tdotholla <kiel.byrne@gmail.com>
2022-02-21 22:07:22 -0500

    mods

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const path = require('path')
 module.exports = {
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
-    NEXT_PUBLIC_OG_KEY: process.env.NEXT_PUBLIC_OG_KEY
+    NEXT_PUBLIC_OG_KEY: process.env.NEXT_PUBLIC_OG_KEY,
+    NEXT_PUBLIC_ATLAS_KEY: process.env.NEXT_PUBLIC_ATLAS_KEY,
+    GOOGLE_SERVER_KEY: process.env.GOOGLE_SERVER_KEY,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
--- src/style/myTheme.ts
+++ src/style/myTheme.ts
@@ -1,6 +1,7 @@
-import { createMuiTheme, colors } from "@material-ui/core";
+import { colors } from "@material-ui/core";
+import { createTheme } from '@material-ui/core/styles'
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       main: colors.orange["A200"],
