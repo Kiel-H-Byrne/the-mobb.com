78e3b32f2b621bc0b6e64ad5130f915556218d11
kiel-h-byrne <kiel.byrne@gmail.com>
2023-08-16 21:52:06 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,8 +149,7 @@ const Home = React.memo(() => {
       </Head>
 
       <main className={style.main}>
-        {!listings ? (
-          // || listings.length === 0
+        {listings.length === 0 ? (
           <LinearProgress />
         ) : (
           <div>
